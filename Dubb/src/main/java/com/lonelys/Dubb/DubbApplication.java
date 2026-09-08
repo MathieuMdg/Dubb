9930a467fa658861d9a63aa67857aba4356dc980
MathieuMdg <mathieu.mendouga-ndzambana@efrei.net>
2026-09-08 16:48:42 +0200

    Initial commit

---
+++ Dubb/src/main/java/com/lonelys/Dubb/DubbApplication.java
@@ -0,0 +1,13 @@
+package com.lonelys.Dubb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubbApplication.class, args);
+	}
+
+}
---
+++ Dubb/src/test/java/com/lonelys/Dubb/DubbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lonelys.Dubb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
