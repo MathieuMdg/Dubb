c73361f1280828fa32a57f6e29c04611592ecb87
Nightfury <goldfury.exe@gmail.com>
2026-09-08 18:21:47 +0200

    added signatures for sql queries

--- Dubb/src/main/java/com/lonelys/Dubb/entity/MovieRepository.java
+++ Dubb/src/main/java/com/lonelys/Dubb/entity/MovieRepository.java
@@ -2,7 +2,11 @@ package com.lonelys.Dubb.entity;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends CrudRepository<Movie, Long> {
 
-
+    List<Movie> findByMovieTitle(String title);
+    List<Movie> findByMovieDirector(String director);
+    List<Movie> findByMovieYear(int year);
 }
